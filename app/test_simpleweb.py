abc1f8b983ddf2992e2c07f28b702306c7794c22
Eduardo Escarti <eduardo.escarti@xing.com>
2020-05-07 15:19:50 +0200

    Add app and super simple test

---
+++ app/test_simpleweb.py
@@ -0,0 +1,9 @@
+from simpleweb import hello
+
+def test_hello():
+    """
+    GIVEN a any user
+    WHEN a user visits our website
+    THEN he/she should be greeted with a "Hello World!" message
+    """
+    assert hello() == "Hello World!"

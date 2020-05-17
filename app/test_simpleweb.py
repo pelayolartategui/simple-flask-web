c5748fb1e4febfe441b2009f80a0bc6c569eebe6
escarti <escarti@gmail.com>
2020-05-17 19:16:07 +0200

    Test faile (#2)
    * Test faile
    
    * Fixed wrong tests
    
    * Fix message
    
    Co-authored-by: Eduardo Escarti <eduardo.escarti@xing.com>

--- app/test_simpleweb.py
+++
@@ -1,9 +0,0 @@
-from simpleweb import hello
-
-def test_hello():
-    """
-    GIVEN a any user
-    WHEN a user visits our website
-    THEN he/she should be greeted with a "Hello World!" message
-    """
-    assert hello() == "Hello World!"

0910187a19db333d5ae0f147c6baaf82b1c24771
Eduardo Escarti <eduardo.escarti@xing.com>
2020-05-23 20:59:58 +0200

    Try out auto-deploy

--- tests/test_simpleweb.py
+++ tests/test_simpleweb.py
@@ -6,4 +6,4 @@ def test_hello():
     WHEN a user visits our website
     THEN he/she should be greeted with a "Hello World!" message
     """
-    assert hello() == "Hello World!"
+    assert hello() == "Hello Country!"
